e5d9617036519a8e7c4dfee14f01b7ce025df911
Ashish <akumar21519@gmail.com>
2024-11-29 16:26:44 +0530

    Add spdlog v1.15.0 for logging

--- squirrel/core/application.cpp
+++ squirrel/core/application.cpp
@@ -1,5 +1,5 @@
 #include "application.h"
-#include <iostream>
+#include "log.hpp"
 
 namespace Squirrel {
     Application::Application() {};
@@ -8,6 +8,19 @@ namespace Squirrel {
 
     void Application::run() {
             std::cout << "Starting Application!" << std::endl;
+            
+            ::Log::Logger::Init();
+
+            CONSOLE_INFO("Hi! This is an example warn log from core logger");
+            CONSOLE_WARN("Hi! This is an example warn log from core logger");
+            CONSOLE_ERROR("Hi! This is an example warn log from core logger");
+            
+            LOG_TRACE("Hi! This is an example error log from console");
+            LOG_DEBUG("Hi! This is an example error log from console");
+            LOG_INFO("Hi! This is an example error log from console");
+            LOG_WARN("Hi! This is an example error log from console");
+            LOG_ERROR("Hi! This is an example error log from console");
+            
             while (true) {}
     }
 }
